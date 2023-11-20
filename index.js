155ef47202a9eec742fe8aa9e7e0d780d3b237dd
Vaquos <furqonotong@gmail.com>
2023-11-20 23:11:53 +0700

    update soal

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(express.json())
 
 var obj;
 fs.readFile('./soal.json', 'utf8', function (err, data) {
-    if (err) throw err;
-    obj = JSON.parse(data);
+    if (err) throw err
+    obj = JSON.parse(data)
 });
 
 app.post('/', function (req, res) {
@@ -23,14 +23,40 @@ app.post('/', function (req, res) {
     }
 })
 
+app.get('/', function(req,res) {
+    res.send({result: obj.length, status:200})
+})
+
+app.post('/result', function(req,res){
+    const data = req.body
+    let count = 0
+    let benar = 0
+    let salah = 0
+    for(const [key, value] of Object.entries(data)){
+        const question = obj.filter(result => result.no == key)
+        if(value == question[0].answer){
+            benar++
+            count++
+        } else {
+            salah++
+        }
+    }
+    const nilai = count/obj.length * 100;
+    res.send({benar, salah, nilai, status:200})
+})
+
 app.get('/:no', function(req,res) {
-    const { no } = req.params;
-    const data = [];
+    const { no } = req.params
+    const data = []
     const question = obj.filter(result => result.no == no)
     question.forEach(element => {
-        data.push({no: element.no, soal: element.soal})
+        data.push({
+            no: element.no, 
+            soal: element.soal,
+            option: element.option
+        })
     });
-    res.send(data).status(200);
+    res.send(data).status(200)
 })
 
 app.listen(3001)
